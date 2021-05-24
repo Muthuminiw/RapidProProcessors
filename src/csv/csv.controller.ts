af95b08662f4156a53fb805673ed96b833b114d1
Muthumini Waidyarathna <muwaidyarathna@virtusa.com>
2021-05-24 16:43:42 +0530

    file upload edited

--- src/csv/csv.controller.ts
+++ src/csv/csv.controller.ts
@@ -6,44 +6,29 @@ const path = require('path')
 
 @Controller('csv')
 export class CsvController {
-    constructor(@InjectQueue('csv') private readonly csvQueue: Queue) {}
-    uploadedCsvPath:string;
+  constructor(@InjectQueue('csv') private readonly csvQueue: Queue) { }
+  uploadedCsvPath: string;
 
-  //   @Post('upload')
-  // async uploadCsv() {
-  //   console.log('Code Controller started');
-  //   await this.csvQueue.add('upload', {
-  //     file: 'E:/PenProject/AssignmentData/sampledata.csv',
-  //   });
-  // }
 
   @Post('upload')
   @UseInterceptors(FileInterceptor("file", { dest: "./uploads" }))
   async uploadCsv(@UploadedFile() file) {
-    console.log('Code Controller started'+file.path);
+    console.log('Code Controller started' + file.path);
     this.uploadedCsvPath = file.path;
     await this.csvQueue.add('upload', {
-      file:  path.resolve(file.path),
+      file: path.resolve(file.path),
     });
   }
+
   @Get('upload')
- 
   async getUploadCsv() {
-  console.log('This is uploaded file path'+this.uploadedCsvPath);
-  return this.uploadedCsvPath;
+    console.log('This is uploaded file path' + this.uploadedCsvPath);
+    return this.uploadedCsvPath;
 
   }
-  // @Post("upload")
-  //   @UseInterceptors(FileInterceptor("file", { dest: "./uploads" }))
-  //   async transcode(@UploadedFile() file) {
-  //       const path = require('path')
-  //       console.log(file.path);
-  //       await this.dbfileUploadQueue.add('upload', {
-  //       file: path.resolve(file.path),
-  //     });
-  //   }
 
-  
+
+
   @Get()
   findAll() {
     return 'Hello Controller';
